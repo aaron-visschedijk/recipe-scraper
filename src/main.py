8cc8bacf48d7f5ea4fb51993969b6f9e1b15b200
aaron-visschedijk <aaron.visschedijk@gmail.com>
2023-03-28 20:26:38 +0200

    fix: json -> to_json

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ from recipe_scrapers import scrape_me
 def lambda_handler(event, context):
     url = event['url']
     scraper = scrape_me(url, wild_mode=True)
-    return scraper.json()
+    return scraper.to_json()
